b33177b42af925d0f2b97a936fb6524333e5b5f9
Joanna Bielińska <joan.bielinska@gmail.com>
2019-02-11 22:18:27 +0100

    Changed Messages.js class

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,36 +1,40 @@
 import React from 'react';
-
-import Message from './Message';
+import axios from 'axios';
 
 class Messages extends React.Component {
+
+  state = {
+    messages: []
+  }
+
   componentDidUpdate() {
     // There is a new message in the state, scroll to bottom of list
     const objDiv = document.getElementById('messageList');
     objDiv.scrollTop = objDiv.scrollHeight;
   }
+  
+  componentDidMount(){
+    axios.get('https://localhost:44389/api/values/getMessage')
+    .then(res => {
+        const messages = res.data;
+        this.setState({ messages});
+        console.log(this.state.messages);
+    })
+  }
 
   render() {
-    // Loop through all the messages in the state and create a Message component
-    const messages = this.props.messages.map((message, i) => {
-        return (
-          <Message
-            Key={i}
-            Mail={message.Mail}
-            message={message.message}
-            fromMe={message.fromMe} />
-        );
-      });
-
+    const messages = this.state.messages.map((message)=> {
+      return(
+      <div className='message-body'>
+        {message.message1 }
+      </div>);
+    });
     return (
       <div className='messages' id='messageList'>
-        { messages }
-      </div>
+         {messages}
+        </div>
     );
   }
 }
-
-Messages.defaultProps = {
-  messages: []
-};
 
 export default Messages;

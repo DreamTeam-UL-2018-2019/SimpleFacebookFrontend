cac33b385480bb8c306c3132a69b77856c7a8943
Joanna Bielińska <joan.bielinska@gmail.com>
2019-02-12 18:02:27 +0100

    cd

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -4,6 +4,7 @@ import ChatInput from './ChatInput';
 import React from 'react';
 import './ChatApp.css';
 import { PostData } from '../services/PostData';
+import * as jwt from 'jwt-decode';
 
 class ChatApp extends React.Component {
   
@@ -13,10 +14,12 @@ class ChatApp extends React.Component {
     this.sendHandler = this.sendHandler.bind(this);
   }
 
+  messagesEnd = React.createRef()
+
   sendHandler(message)
   {
     const messageObject = {
-      idSender: 1,
+      idSender: this.getUserId,
       idReceiver:2,
       message1: message,
       date: new Date().toLocaleString(),
@@ -26,13 +29,24 @@ class ChatApp extends React.Component {
     this.addMessage(messageObject);
   }
 
-  //getUserId() {
-   // var jwtDecode = require('jwt-decode');
-    //const token = sessionStorage.getItem("token");
-    //var userId = jwtDecode(token).user.idSender;
-   // console.log(userId);
-   // return userId;
- // }
+  getUserId() {
+    const token = sessionStorage.getItem("token");
+    var userId = jwt(token).user.idSender;
+    console.log(userId);
+    return userId;
+  }
+
+  componentDidMount(){
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate(){
+    this.scrollToBottom();
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.current.scrollIntoView({ behavior: 'smooth'})
+  }
 
   addMessage(message) 
   {
@@ -43,8 +57,10 @@ class ChatApp extends React.Component {
     return (
       <div className="container" >
         <h3>Chat</h3>
+        <div className="container" ref={this.messagesEnd}>
         <Messages messages={this.state.messages} />
         <ChatInput onSend={this.sendHandler} />
+        </div>
       </div>
     );
   }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -6,28 +6,27 @@ class Messages extends React.Component {
   state = {
     messages: []
   }
-
+  
   componentDidUpdate() {
-    // There is a new message in the state, scroll to bottom of list
-    const objDiv = document.getElementById('messageList');
-    objDiv.scrollTop = objDiv.scrollHeight;
+    axios.get('https://localhost:44389/api/values/getMessage')
+    .then(res => {
+        const messages = res.data;
+        this.setState({ messages});
+    })
   }
-
 
   componentDidMount(){
     axios.get('https://localhost:44389/api/values/getMessage')
     .then(res => {
         const messages = res.data;
         this.setState({ messages});
-        console.log(this.state.messages);
     })
   }
 
   render() {
     const messages = this.state.messages.map((message)=> {
       return(
-
-        <div className='listOfMessages'>
+        <div className='listOfMessages' >
             <div className='Mail'>
               { this.props.Mail }
             </div>
@@ -35,11 +34,10 @@ class Messages extends React.Component {
               {message.message1 }
             </div>
       </div>);
-      
     });
     return (
-      <div className='messages' id='messageList'>
-         {messages}
+      <div className='messages' >
+          {messages}
         </div>
     );
   }
